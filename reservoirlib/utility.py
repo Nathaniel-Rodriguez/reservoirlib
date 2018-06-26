11a0f085e8fd000f69b9905de7d195a6171e0890
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2018-06-25 22:35:04 -0400

    added save and load pickle functions to utility

--- reservoirlib/utility.py
+++ reservoirlib/utility.py
@@ -1,8 +1,26 @@
 import numpy as np
+import pickle
 
 
 DEFAULT_INT = np.int64
 DEFAULT_FLOAT = np.float64
+
+
+def load(filename):
+    pickled_obj_file = open(filename, 'rb')
+    obj = pickle.load(pickled_obj_file)
+    pickled_obj_file.close()
+
+    return obj
+
+
+def save(obj, filename):
+    """
+    objectives and their args are not saved with the ES
+    """
+    pickled_obj_file = open(filename, 'wb')
+    pickle.dump(obj, pickled_obj_file, protocol=pickle.DEFAULT_PROTOCOL)
+    pickled_obj_file.close()
 
 
 def normalize_root_mean_squared_error(residuals, ymax, ymin):
